525bf2a130c83ef88a2a2cdbc837fcc208844210
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-10-30 22:27:45 -0300

    Desafio008

---
+++ ex008.py
@@ -0,0 +1,7 @@
+metro = int(input('digite o valor em metro: '))
+
+centimetro = metro * 100
+
+milimetros = metro * 1000
+
+print('Metros convertido para Centimetros é: {}cm & Metros convertido para Milimetros é: {}mm'.format(centimetro,milimetros))
