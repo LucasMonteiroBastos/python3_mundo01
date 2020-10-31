79a52c19723c942ab6c597fdd3f16bc3de73b88c
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-10-30 22:25:54 -0300

    Desafio006

---
+++ ex006.py
@@ -0,0 +1,9 @@
+x = int(input('digite um numero: '))
+
+dobro  = x * 2
+
+triplo = x * 3
+
+raizQuadadrada = pow(x,1/2)
+
+print('O dobro do valor é {} e o triplo do valor é {} e a Raiz Quadrada é {:.3}'.format(dobro,triplo,raizQuadadrada))
