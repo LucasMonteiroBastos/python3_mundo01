ac0dc56ae39f2305baf1b6e0dabe6342860d818a
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-10-30 22:26:45 -0300

    Desafio007

---
+++ ex007.py
@@ -0,0 +1,6 @@
+nota1 = int(input('digite a primeira nota do aluno: '))
+nota2 = int(input('digite a segunda nota do aluno: '))
+
+media = (nota1 + nota2) / 2
+
+print('A media do aluno é: {}'.format(media))
