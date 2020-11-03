15b2fe7e9931495ce43008f60a4cb1d66740779e
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-11-02 22:57:12 -0300

    Desafio016

---
+++ ex016.py
@@ -0,0 +1,8 @@
+from math import trunc
+num = float(input('digite um valor: '))
+print('O valor digitado foi: {} E aparte inteiro do valor é {}'.format(num,   trunc(num)))
+
+#num = float(input('Digite um valor: '))
+#print('O valor digitado foi: {} e sua porção é: {}'.format(num, int(num)))
+
+
