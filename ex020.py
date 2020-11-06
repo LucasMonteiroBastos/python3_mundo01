bf71cf14007627645be11253378a72b510c4e2b3
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-11-06 16:45:54 -0300

    Desafio020

---
+++ ex020.py
@@ -0,0 +1,17 @@
+#Exercício Python 20:
+#O mesmo professor do desafio 19 quer sortear a ordem de apresentação de trabalhos dos alunos.
+#Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+
+import random
+
+a = str(input('Aluno 1: '))
+b = str(input('Aluno 2: '))
+c = str(input('Aluno 3: '))
+d = str(input('Aluno 4: '))
+
+lista = [a,b,c,d]
+random.shuffle(lista)
+
+print('A ordem da apresentação será...')
+print(lista)
