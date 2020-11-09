557b0d30cb64becc5d3159661231a03f1a5b0da5
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-11-09 15:41:57 -0300

    Desafio030

---
+++ ex030.py
@@ -0,0 +1,9 @@
+'''Exercício Python 30:
+Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.'''
+
+num = int(input('Numero Inteiro: '))
+
+if num % 2 == 0:
+    print('PAR')
+else:
+    print('IMPAR')
