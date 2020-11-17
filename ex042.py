e0d6837dd813da79e7047a98cf62c9352b52babd
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2020-11-17 13:44:22 -0300

    Desafio042

--- ex042.py
+++ ex042.py
@@ -14,9 +14,13 @@ a = float(input('Primeiro segmento: '))
 b = float(input('Segundo segmento: '))
 c = float(input('Terceiro segmento: '))
 
-if a == b and a == c and b == c:
-    print('Os segmentos formam um triangulo EQUILÁTERO.')
-elif a == b and a != c and b != c and a != b and a != c and b == c:
-    print('Os segmentos formam um triangulo ISÓSCELES.')
-elif a != b and a != c and b != c:
-    print('Os segmentos formam um triangulo ESCALENO.')
+if a < b + c and b < a + c and  c < a + b:
+    print('Os segmentos acima PODEM FORMA um triângulo ', end='')
+    if a == b == c:
+        print('EQUILÁTERO.')
+    elif a != b != c != a:
+        print('ESCALENO.')
+    else:
+        print('ISÓSCELES')
+else:
+    print('Os segmentos acima não FORMAM UM TRIANGULO.')
