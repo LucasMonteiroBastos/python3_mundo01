ffe5bb7dbee280cbae633d66fad13387bc945682
LucasMonteiroBastos <monteiro.bastos1994@gmail.com>
2021-01-04 22:02:52 -0300

    Estudando For

---
+++ teste.py
@@ -0,0 +1,20 @@
+'''n = int(input('Digite um numero: '))
+for a in range (0,n):
+    print(a)
+print('fim')'''
+
+
+'''i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+
+for c in range(i, f+1, p):
+    print(c)
+print(c)'''
+
+
+soma = 0
+for c in range(0,3):
+    n = int(input('Digite um valor: '))
+    soma += n
+print('O somatorio de todos os valores foi {}'.format(soma))
